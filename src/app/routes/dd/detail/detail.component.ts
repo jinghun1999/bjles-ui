cebc3e70c9fff56dd3e47ea2e517c037efd0ae0c
lsdajing <lsdajing@126.com>
2019-09-04 12:09:32 +0800

    排版及增加一些方法

--- src/app/routes/dd/detail/detail.component.ts
+++ src/app/routes/dd/detail/detail.component.ts
@@ -2,45 +2,46 @@ import { NzMessageService, NzModalRef } from 'ng-zorro-antd';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
-import { STColumn } from '@delon/abc';
+import { STColumn, STWidthMode } from '@delon/abc';
 
 @Component({
   selector: 'app-dd-detail',
   templateUrl: './detail.component.html',
 })
 export class DdDetailComponent implements OnInit {
-  @Input() record: any;
   constructor(
     private modal: NzModalRef,
     public msgSrv: NzMessageService,
     public http: _HttpClient,
     public msg: NzMessageService,
   ) {}
+  size: 'small';
+  record: any = {};
+  i: any;
   dataDetail: any[] = [];
-  dataBasicInfo: any[] = [];
+  widthMode: STWidthMode = {};
   columnsDetail: STColumn[] = [
-    { title: '零件序号', index: '' },
-    { title: '拉动方式', index: '', sort: true },
-    { title: '零件号', index: 'part_code', sort: true },
-    { title: '零件名称', index: 'part_name', sort: true },
-    { title: '需求箱数', index: 'required_pack_count', sort: true },
-    { title: '整包装数', index: 'pack_size', sort: true },
-    { title: '包装型号', index: 'pack_type', sort: true },
-    { title: '需求件数', index: 'required_part_count', sort: true },
-    { title: '发货箱数', index: 'pack_count', sort: true },
-    { title: '发货件数', index: 'part_count', sort: true },
-    { title: '实际箱数', index: 'actual_pack_count', sort: true },
-    { title: '实际件数', index: 'actual_part_count', sort: true },
-    { title: '库位', index: 'dloc', sort: true },
-    { title: '工位', index: 'uloc', sort: true },
-    { title: '源库位地址', index: 'rdc_dloc', sort: true },
+    { title: '零件序号', type: 'no', width: 60 },
+    { title: '拉动方式', index: 'interface_types' },
+    { title: '零件号', index: 'part_code' },
+    { title: '零件名称', index: 'part_name' },
+    { title: '需求箱数', index: 'required_pack_count' },
+    { title: '整包装数', index: 'pack_size' },
+    { title: '包装型号', index: 'pack_type' },
+    { title: '需求件数', index: 'required_part_count' },
+    { title: '发货箱数', index: 'actual_send_pack_qty' },
+    { title: '发货件数', index: 'actual_send_part_qty' },
+    { title: '实际箱数', index: 'actual_pack_count' },
+    { title: '实际件数', index: 'actual_part_count' },
+    { title: '库位', index: 'dloc' },
+    { title: '工位', index: 'uloc' },
+    { title: '源库位地址', index: 'rdc_dloc' },
   ];
 
   ngOnInit() {
     this.http.get('/dd/GetRunSheetDetail?runsheet_id=' + this.record.runsheet_id).subscribe(
       res => {
-        this.dataBasicInfo = res.data.basicinfo;
-        this.dataDetail = res.data.detail;
+        this.dataDetail = res.data;
       },
       (err: any) => this.msg.error('获取不到物料单明细信息！！'),
     );
