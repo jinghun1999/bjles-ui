b1a3dcd99f9a05bb6f386a93fc7e6269eff7b515
lsdajing <lsdajing@126.com>
2019-09-11 19:26:47 +0800

    零件在途库存

--- src/app/routes/part/partstocadjust/partstocadjust.component.ts
+++ src/app/routes/part/partstocadjust/partstocadjust.component.ts
@@ -1,45 +1,238 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc';
-import { SFSchema } from '@delon/form';
+import { STColumn, STComponent, STData, STPage, STChange, XlsxService } from '@delon/abc';
+import { CommonFunctionService, CommonApiService } from '@core';
+import { NzMessageService } from 'ng-zorro-antd';
+import { PageInfo, SortInfo, ItemData } from 'src/app/model';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-part-partstocadjust',
   templateUrl: './partstocadjust.component.html',
 })
 export class PartPartstocadjustComponent implements OnInit {
-  url = `/user`;
-  searchSchema: SFSchema = {
-    properties: {
-      no: {
-        type: 'string',
-        title: '编号'
-      }
-    }
-  };
+  actionPath = 'PartManagement/PartStockAdjust.aspx';
+  searchPath = '/part/GetOnlineStockList';
   @ViewChild('st', { static: false }) st: STComponent;
   columns: STColumn[] = [
-    { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
-    { title: '头像', type: 'img', width: '50px', index: 'avatar' },
-    { title: '时间', type: 'date', index: 'updatedAt' },
-    {
-      title: '',
-      buttons: [
-        // { text: '查看', click: (item: any) => `/form/${item.id}` },
-        // { text: '编辑', type: 'static', component: FormEditComponent, click: 'reload' },
-      ]
-    }
+    { title: '工厂', index: 'plant', sort: true },
+    { title: '车间', index: 'workshop', sort: true },
+    { title: '库位编号', index: 'dloc', sort: true },
+    { title: '在途库存（箱数）', index: 'online_boxes', sort: true },
+    { title: '零件号', index: 'part_no', sort: true },
+    { title: '零件名称', index: 'part_cname', sort: true },
+    { title: '供应商代码', index: 'supplierId', sort: true },
+    { title: '供应商名称', index: 'supplier_name', sort: true },
   ];
+  selectedRows: STData[] = [];
+  pages: STPage = {
+    total: '', // 分页显示多少条数据，字符串型
+    show: true, // 显示分页
+    front: false, // 关闭前端分页，true是前端分页，false后端控制分页
+    showSize: true,
+    pageSizes: [10, 30, 50, 100],
+  };
+  expandForm = true;
+  loading: boolean;
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
+  size = 'small';
+  q: any = {
+    page: new PageInfo(),
+    sort: new SortInfo(),
+    plant: '',
+    workshop: [],
+  };
+  data: any[] = [];
+  data_import: any;
+  dataAction: any[] = [];
+  pre_lists = [];
+  sub_workshops = [];
 
-  ngOnInit() { }
+  constructor(
+    private http: _HttpClient,
+    private modal: ModalHelper,
+    public msg: NzMessageService,
+    private cdr: ChangeDetectorRef,
+    private capi: CommonApiService,
+    private cfun: CommonFunctionService,
+    private xlsx: XlsxService,
+  ) {}
 
-  add() {
-    // this.modal
-    //   .createStatic(FormEditComponent, { i: { id: 0 } })
-    //   .subscribe(() => this.st.reload());
+  ngOnInit() {
+    this.loading = true;
+
+    this.capi.getPlant().subscribe(
+      (res: any) => {
+        this.pre_lists = res;
+        if (this.pre_lists.length > 0) {
+          this.sub_workshops = this.pre_lists[0].children;
+          this.q.plant = this.pre_lists[0].value;
+          this.plantChange(this.q.plant);
+        }
+      },
+      (err: any) => this.msg.error('获取不到工厂车间信息！！'),
+    );
+
+    // toolBar
+    this.capi.getActions(this.actionPath).subscribe((res: any) => {
+      this.dataAction = res;
+    });
+
+    this.loading = false;
   }
 
+  getCodeDetails(value: any, type: any) {
+    // tslint:disable-next-line: no-eval
+    const tmp_data = eval('this.sub_' + type);
+    if (value) {
+      // tslint:disable-next-line: no-eval
+      if (tmp_data.data.length === 0)
+        this.capi.getCodeDetailInfo(type, '').subscribe((res: any) => {
+          tmp_data.data = res;
+        });
+    }
+  }
+
+  plantChange(value: string): void {
+    const l = this.pre_lists.find(p => p.value === value);
+    this.sub_workshops = l.children;
+    this.q.workshop = '';
+  }
+
+  stChange(e: STChange) {
+    switch (e.type) {
+      case 'checkbox':
+        this.selectedRows = e.checkbox!;
+        this.cdr.detectChanges();
+        break;
+      case 'filter':
+        this.getData();
+        break;
+      case 'pi':
+        this.q.page.pi = e.pi;
+        this.getData();
+        break;
+      case 'ps':
+        this.q.page.ps = e.ps;
+        this.getData();
+        break;
+      case 'sort':
+        this.q.sort.field = e.sort.column.indexKey;
+        this.q.sort.order = e.sort.value;
+        this.getData();
+        break;
+    }
+  }
+
+  toolBarOnClick(e: any) {
+    // tslint:disable-next-line:no-debugger
+    // debugger;
+    switch (e.action_name) {
+      case 'Search':
+        this.search();
+        this.expandForm = false;
+        break;
+      case 'Export':
+        this.export();
+        break;
+      case 'HideOrExpand':
+        this.hideOrExpand();
+        break;
+      case 'Create':
+        this.Create();
+        break;
+    }
+  }
+
+  hideOrExpand() {
+    this.expandForm = !this.expandForm;
+  }
+
+  Create() {
+    // this.modal.create(PartPartcardlistEditComponent, { record: [] }, { size: 'xl' }).subscribe(res => {});
+  }
+
+  export() {
+    if (this.st.total === 0) {
+      this.msg.error('请输入条件，查询出数据方可导出数据！');
+      return false;
+    }
+
+    this.q.page.export = true;
+    this.http
+      .post(this.searchPath, this.q)
+      .pipe(tap(() => (this.loading = false)))
+      .subscribe(
+        res => {
+          if (res.successful) {
+            this.st.export(res.data.rows, {
+              callback: this.cfun.callbackOfExport,
+              filename: 'result.xlsx',
+              sheetname: 'sheet1',
+            });
+          } else {
+            this.msg.error(res.message);
+            this.loading = false;
+          }
+        },
+        (err: any) => this.msg.error('系统异常'),
+      );
+
+    this.q.page.export = false;
+  }
+
+  search() {
+    this.getData();
+  }
+
+  getData() {
+    this.loading = true;
+    const tmp_workshops = this.sub_workshops.map(p => p.value);
+
+    if (this.q.workshop === '' || this.q.workshop === undefined || this.q.workshop.length === 0) {
+      this.q.workshop = tmp_workshops;
+    }
+
+    this.http
+      .post(this.searchPath, this.q)
+      .pipe(tap(() => (this.loading = false)))
+      .subscribe(
+        res => {
+          if (res.successful) {
+            this.data = res.data.rows;
+            this.st.total = res.data.total;
+            this.cdr.detectChanges();
+          } else {
+            this.msg.error(res.message);
+            this.loading = false;
+          }
+        },
+        (err: any) => this.msg.error('系统异常'),
+      );
+    if (tmp_workshops === this.q.workshop) this.q.workshop = [];
+  }
+
+  getListItems(value: any, type: any): void {
+    // tslint:disable-next-line: no-eval
+    const tmp_data = eval('this.sub_' + type);
+
+    if (value && this.q.workshop.toString() !== tmp_data.last_workshop) {
+      if (this.q.workshop.length > 0) {
+        this.loading = true;
+        this.capi.getListItems(type, this.q.plant, this.q.workshop.toString()).subscribe(
+          (res: any) => {
+            tmp_data.data = res;
+          },
+          (err: any) => this.msg.error('获取数据失败!'),
+        );
+        this.loading = false;
+        tmp_data.last_workshop = this.q.workshop.toString();
+      } else {
+        tmp_data.data = [];
+        tmp_data.last_workshop = '';
+      }
+      // tslint:disable-next-line: no-eval
+      eval('this.q.' + type + ' =  [];');
+    }
+  }
 }
